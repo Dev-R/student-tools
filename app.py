f146d88e23802c7924976ed72f638fbd3890b146
Rabi <83784102+Dev-R@users.noreply.github.com>
2021-12-30 04:17:36 +0800

    Added Azure BLOB fixed minor bugs

--- app.py
+++ app.py
@@ -10,43 +10,50 @@ from werkzeug.exceptions import default_exceptions, HTTPException, InternalServe
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 from helpers import allowed_file, get_text_OCR, apology, login_required, get_image_path
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
 from PIL import Image
 from datetime import datetime
+
+#Retrieve the connection string
+connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+AZURE_STORGE_ACCOUNT = "studenttools1 "
+CONTAINER_NAME =  "img-uploads"
 
 # Get Local time
 LOCAL_TIMEZONE = (datetime.now()).strftime("%m-%d-%Y, %H:%M:%S")
 # Set to the full path of the upload file in static
-UPLOAD_FOLDER = '/app/static/uploads'
-#  Set to the full path of the current Project Directory 
-CURRENT_PROJECT_DIR = '/app'
+UPLOAD_FOLDER = '/home/ubuntu/project/static/uploads'
+#  Set to the full path of the current Project Directory
+CURRENT_PROJECT_DIR = '/home/ubuntu/project'
 
 # Configure application
 app = Flask(__name__)
-app.secret_key = 'A0AKR5TGD\ R~XHH!jmN]LWX/,?RT'
-# app.config['SECRET_KEY'] = os.urandom(24)
-# this is important or wont work
-app.config['SESSION_COOKIE_NAME'] = "my_session"
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 # Configure Database
-db = SQL(os.getenv("DATABASE_URL"))
+db = SQL("sqlite:///site.db")
 # Membership types
 MEMBER_SHIP = {'A': 'ADMIN', 'G': 'GUEST'}
-# redis configuration 
-#redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-#redis = redis.from_url(redis_url)
 
 
 """Configure session to use filesystem (instead of signed cookies) and other app configuration"""
+
+
 # The directory where session files are stored.
 # app.config["SESSION_FILE_DIR"] = mkdtemp()
 # Whether use permanent session or not, default to be True
 app.config["SESSION_PERMANENT"] = False
 # Specifies which type of session interface to use.
 app.config["SESSION_TYPE"] = "filesystem"
+
+
+
 Session(app)
 # Configure uploading file
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 
 # Ensure responses aren't cached
 @app.after_request
@@ -64,14 +71,16 @@ def redirect_homepage():
 # Render index
 @app.route('/index', methods=["GET", "POST"])
 def homepage():
-    username = check_login_status()
-    return render_template('index.html',  user=username, session=session)
+    # username = check_login_status()
+    # print(username)
+    return render_template('index.html',  user=check_login_status(), session=session)
 
 
 # Redirect to index
 @app.route('/about', methods=["GET", "POST"])
 def about():
     return render_template('about.html')
+
 
 # Redirect to signup
 @app.route('/signup', methods=["GET", "POST"])
@@ -86,31 +95,30 @@ def signup():
         # Check if username is valid and a string
         if not username or not isinstance(username, str):
             error = "Please enter a valid user name"
-            
+
         # Check for password errors
         elif not password:
             error = "Please enter a valid password"
-        
+
         # Check if password confirmation match
         elif password != password_confirm:
-            error ="Password confirmation doesn't match"
-        
+            error = "Password confirmation doesn't match"
+
         # TO-DO: password len > 10:
 
         # Check for matching username
-        elif db.execute("SELECT * FROM users WHERE username = ?" , username):
+        elif db.execute("SELECT * FROM users WHERE username = ?", username):
             error = "Username taken"
-            
+
         if error is None:
             # Add user to data base
             add_user_to_db(username, password)
             flash("Registered 😁")
             return render_template('index.html')
-            
+
         flash(error)
         return render_template("signup.html")
-        
-        
+
     else:
         return render_template('signup.html')
 
@@ -127,27 +135,28 @@ def login():
         error = None
         # Ensure username was entered
         if not request.form.get('username'):
-            error  = "must provide username"
+            error = "must provide username"
 
         # Ensure password was entered
         elif not request.form.get('password'):
-            error  = "must provide password"
+            error = "must provide password"
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", username)
-        
+
         # Check if username exist and password is correct in DB
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             error = 'Invalid credentials'
-            
+
         if error is None:
-           # Remember which user has logged in
-           session["user_id"] = rows[0]["id"]
-           return render_template('index.html', user=username)
-          
+            # Remember which user has logged in
+            session["user_id"] = rows[0]["id"]
+            print(session)
+            return render_template('index.html', user=username, session=session)
+
         flash(error)
         return render_template("login.html")
-        
+
     else:
         # Redirect to login page
         return render_template('login.html')
@@ -158,7 +167,7 @@ def text_reader():
     """Redirect User to text reader page"""
     if request.method == "POST":
         return render_template('text_reader.html')
-        
+
     else:
         return render_template('text_reader.html')
 
@@ -166,14 +175,15 @@ def text_reader():
 @app.route('/ocr', methods=["GET", "POST"])
 def ocr():
     if request.method == "POST":
-        """Process image user uploaded"""
+        """Process OCR image user uploaded"""
         error = None
         # Check if user uploaded a valid file
         if 'file' not in request.files:
-            error ='No file part'
+            error = 'No file part'
             flash(error)
-            return render_template('ocr.html') 
-    
+            return render_template('ocr.html')
+
+
         # Get user file and Store it
         file = request.files['file']
         # Check if filename is not malicious
@@ -184,10 +194,10 @@ def ocr():
             filename = str(uuid.uuid4())+".jpeg"
             # Store image in the UPLOAD_FOLDER dir
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        
+            
         # Extract text in image using Microsoft ComputerVision SDK
         ocr_text = get_text_OCR(filename)
-        
+        "TEST BLOB presisting"
         # If the user is logged in, store the file in his db
         if session:
             remember_user_ocr_file(filename, ocr_text)
@@ -208,7 +218,7 @@ def history():
 
 
 # TO-DO rate_limiter
-""" 
+"""
 @app.route('/reader', methods=["GET", "POST"])
 def rate_limiter():
     if request.method == "POST":
@@ -235,44 +245,69 @@ def logout():
     return redirect("/")
 
 
-def remember_user_ocr_file(filename, ocr_text)-> None:
+def remember_user_ocr_file(filename, ocr_text) -> None:
     """Remeber what image user uploaded for History"""
-    db.execute("INSERT INTO ocr (user_id, img_dir, img_text, execution_date) VALUES(?, ?, ?, ?)", session['user_id'], 
-               get_image_path(filename).replace(CURRENT_PROJECT_DIR, ""), ocr_text, str(LOCAL_TIMEZONE))
-    
-def check_login_status() -> bool:
-    """Check if user logged in"""
-    if session:
-        username = db.execute("SELECT username FROM users WHERE id = ?", session['user_id'])[0]['username']
-        if username:
-            return username
-        return False
-    
+    image_url = upload_blob(filename)
+    print(image_url)
+    db.execute("INSERT INTO ocr (user_id, img_dir, img_text, execution_date) VALUES(?, ?, ?, ?)", session['user_id'],
+               image_url, ocr_text, str(LOCAL_TIMEZONE))
+
+
 def errorhandler(error):
     """Handle error"""
     if not isinstance(error, HTTPException):
         error = InternalServerError()
         error = str(error.name) + str(error.code)
     if error:
-        code =  str(error.code)
-        error = 'Error: ' + code + ' Reported, Please Try again' 
+        code = str(error.code)
+        error = 'Error: ' + code + ' Reported, Please Try again'
         # TO-DO: Create an Error table in database to keep track of user errors
-        #If internal error, logout user
         if code == "500":
             session.clear()
         flash(error)
         return render_template('index.html')
 
 
+def check_login_status() -> bool:
+    """check if user logged in"""
+    if session:
+        username_logged = db.execute("SELECT username FROM users WHERE id = ?", session['user_id'])[0]['username']
+        if username_logged:
+            return username_logged
+            
+        return False
+
+
 def add_user_to_db(username, password) -> None:
     """Add user to database"""
-    db.execute("INSERT INTO users (username, hash, member_ship,registration_date) VALUES(?, ?, ?, ?)", username, generate_password_hash(password), MEMBER_SHIP['A'], 
+    db.execute("INSERT INTO users (username, hash, member_ship,registration_date) VALUES(?, ?, ?, ?)", username, generate_password_hash(password), MEMBER_SHIP['A'],
                str(LOCAL_TIMEZONE))
-    
+               
+
 # Listen for errors
 for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
 
-if __name__ == "__main__":
- port = int(os.environ.get("PORT", 8080))
- app.run(host="0.0.0.0", port=port)
+
+def upload_blob(filename) -> str:
+    """Upload file to Azure blob container and return URL"""
+    try:
+        # Create the BlobServiceClient object which will be used to create a container client
+        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+        # Create a unique name for the container
+        container_name = CONTAINER_NAME
+        upload_file_path = get_image_path(filename)
+        # Create a blob client using the local file name as the name for the blob
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=filename)
+        # Upload the created file
+        with open(upload_file_path, "rb") as data:
+            blob_client.upload_blob(data)
+        # return file blob url
+        file_url = "https://" + AZURE_STORGE_ACCOUNT + ".blob.core.windows.net" + '/' + CONTAINER_NAME + '/' + filename
+        return file_url.replace(" ", "")
+        
+        
+    except Exception as ex:
+        print('Exception:')
+        print(ex)
+        print("filename" + filename)
